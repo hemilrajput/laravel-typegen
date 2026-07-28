8e436ae004daadad061fb0af6f0870bf7eac0cd0
Hemil Rajput <hemilrajput.2004@gmail.com>
2026-07-29 01:06:09 +0530

    feat(docs): make copyright year dynamic in footer

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -80,7 +80,7 @@ export default defineConfig({
 
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2024 Hemil Rajput',
+      copyright: `Copyright © 2024-${new Date().getFullYear()} Hemil Rajput`,
     },
 
     docFooter: {
